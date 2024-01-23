5a66aefe63bfd49eab9be48cd73f39ac768c6bc5
ashwath462 <ashwatharora462@gmail.com>
2024-01-24 04:05:49 +0530

    base layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,4 +3,30 @@
     import Navbar from '../components/navbar.svelte';
 </script>
 
-<Navbar />
+<div class="bg-slate-300 h-[100vh]">
+    <Navbar />
+    <div class="mt-8 max-md:space-x-2 md:space-x-4 flex place-content-center">
+        <input type="text" placeholder="Type here" class="input input-bordered input-primary w-full max-w-[50%] max-md:btn-sm" />
+        <button class="btn btn-secondary max-md:btn-sm">Add task</button>
+        <button class="btn btn-error max-md:btn-sm">Remove All</button>
+    </div>
+
+    <div class="mt-8 w-full flex place-content-center">
+            <div class="flex flex-col w-[50%] text-center">
+                <p class="text-2xl text-teal-950 font-black">To Do</p>
+                <span>hi how are you</span>
+                <span>hi how are you</span>
+                <span>hi how are you</span>
+                <span>hi how are you</span>
+                <span>hi how are you</span>
+            </div>
+            <div class="flex flex-col w-[50%] text-center">
+                <p class="text-2xl text-blue-950 font-black ">Done</p>
+                <span>hi how are you</span>
+                <span>hi how are you</span>
+                <span>hi how are you</span>
+                <span>hi how are you</span>
+                <span>hi how are you</span>
+            </div>
+    </div>
+</div>
